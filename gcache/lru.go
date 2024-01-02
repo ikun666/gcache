1136ba9ce23fd867e45e7338cd999194fe77a25a
ikun666 <2922098031@qq.com>
2024-01-02 22:32:05 +0800

    add lru

---
+++ gcache/lru.go
@@ -0,0 +1,75 @@
+package gcache
+
+import "container/list"
+
+type LRUCache struct {
+	maxBytes int64
+	nbytes   int64
+	list     *list.List
+	cache    map[string]*list.Element
+	// 删除时的回调函数
+	OnRemove func(key string, value Value)
+}
+
+//链表键值对
+type entry struct {
+	key   string
+	value Value
+}
+
+// 获取value值内存大小
+type Value interface {
+	Len() int
+}
+
+func NewLRUCache(maxBytes int64, OnRemove func(key string, value Value)) *LRUCache {
+	return &LRUCache{
+		maxBytes: maxBytes,
+		list:     list.New(),
+		cache:    make(map[string]*list.Element),
+		OnRemove: OnRemove,
+	}
+}
+func (c *LRUCache) Get(key string) (value Value, ok bool) {
+	//如果有将其移到队头  队头元素常用 队尾元素不常用
+	if ele, ok := c.cache[key]; ok {
+		kv := ele.Value.(*entry)
+		c.list.MoveToFront(ele)
+		return kv.value, true
+	}
+	return
+}
+func (c *LRUCache) RemoveOldest() {
+	//删除队尾元素 以其映射 修改容量 执行回调
+	ele := c.list.Back()
+	if ele != nil {
+		kv := ele.Value.(*entry)
+		c.list.Remove(ele)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnRemove != nil {
+			c.OnRemove(kv.key, kv.value)
+		}
+	}
+}
+
+func (c *LRUCache) Add(key string, value Value) {
+	//如同存在，就将其移到队头  更新新值大小
+	if ele, ok := c.cache[key]; ok {
+		kv := ele.Value.(*entry)
+		c.list.MoveToFront(ele)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else { //不存在插入队头
+		ele := c.list.PushFront(&entry{key, value})
+		c.cache[key] = ele
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	//插入后 如果超过容量 删除最近最久未使用的元素直到不超过容量
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+func (c *LRUCache) Len() int {
+	return c.list.Len()
+}
---
+++ gcache/lru_test.go
@@ -0,0 +1,61 @@
+package gcache_test
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/ikun666/gcache"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+func TestGet(t *testing.T) {
+	lru := gcache.NewLRUCache(int64(100), nil)
+	lru.Add("key1", String("1234"))
+	fmt.Println(lru.Get("key1"))
+	fmt.Println(lru.Get("key2"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit key1=1234 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
+
+func TestRemoveoldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "k3"
+	v1, v2, v3 := "value1", "value2", "v3"
+	cap := len(k1 + k2 + v1 + v2)
+	lru := gcache.NewLRUCache(int64(cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+	fmt.Println(lru.Get("key1"))
+	fmt.Println(lru.Get("key2"))
+	fmt.Println(lru.Get("k3"))
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
+}
+
+func TestOnRemove(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value gcache.Value) {
+		keys = append(keys, key)
+	}
+	lru := gcache.NewLRUCache(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+
+	expect := []string{"key1", "k2"}
+	fmt.Println(keys)
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call OnEvicted failed, expect keys equals to %s", expect)
+	}
+}
